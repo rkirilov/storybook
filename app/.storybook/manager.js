1c9b49a0b43321d162eaf91ec7e037767e8f5ae7
Radoslav Kirilov <radoslav.kirilov@telerik.com>
2021-02-15 18:04:20 +0200

    Add theme

--- app/.storybook/manager.js
+++ app/.storybook/manager.js
@@ -1,14 +1,19 @@
 import { addons } from '@storybook/addons';
+import yourTheme from './customTheme';
 
 addons.setConfig({
-  previewTabs: {
-    'storybook/docs/panel': { index: -1 },
-    'storybook/canvas/panel': {
-      hidden: true
-    },
-    canvas: {
-      title: 'Story',
-      hidden: false,
-    },
-  },
+  theme: yourTheme,
 });
+
+// addons.setConfig({
+//   previewTabs: {
+//     'storybook/docs/panel': { index: -1 },
+//     'storybook/canvas/panel': {
+//       hidden: true
+//     },
+//     canvas: {
+//       title: 'Story',
+//       hidden: false,
+//     },
+//   },
+// });
