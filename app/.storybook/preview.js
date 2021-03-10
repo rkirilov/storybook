7c1e4f599dac1ae4187cea661be9300b4bbfa835
Radoslav Kirilov <radoslav.kirilov@telerik.com>
2021-03-10 13:47:06 +0200

    Apply sort order of stories

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,3 +1,10 @@
+import "../src/index.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      order: ['Example', 'Components', ['Button', ['Solid', 'Flat']]],
+    },
+  },
 }
