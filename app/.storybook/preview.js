75164c5a87a66416249d6bc845a7be3a87a17744
Radoslav Kirilov <radoslav.kirilov@telerik.com>
2021-03-10 16:28:36 +0200

    Add design system

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,4 +1,5 @@
-import "../src/index.css";
+import '!style-loader!css-loader!sass-loader!../src/index.scss';
+import "../src/index.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
